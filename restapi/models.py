ba1e2e03bdf0ab3155f425420c83e017ab913b5f
PMDRose <shiro@ppy.sh>
2018-02-28 13:23:14 +0100

    add get method for rooms page

--- restapi/models.py
+++ restapi/models.py
@@ -9,3 +9,16 @@ class GameModel(models.Model):
     dice = models.CharField(max_length=32, null=False, blank=False)
     created_at = models.DateTimeField(default=datetime.datetime.utcnow(), editable=False)
     updated_at = models.DateTimeField(default=datetime.datetime.utcnow())
+
+    def __str__(self):
+        return self.name + '(' + self.dice + ')'
+
+class Room(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField(null=True, blank=True)
+    max_players = models.IntegerField(default=8)
+    created_at = models.DateTimeField(default=datetime.datetime.utcnow(), editable=False)
+    updated_at = models.DateTimeField(default=datetime.datetime.utcnow())
+
+    owner = models.ForeignKey(User)
+    system = models.ForeignKey(GameModel)
--- restapi/views.py
+++ restapi/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from restapi import models as rp_models
+import json
 
 # Create your views here.
+def rooms(request):
+    if request.method=='GET':
+        response=[]
+        room_list=list(rp_models.Room.objects.filter())
+        for room in room_list:
+            room_dic={}
+            room_dic['id']=room.id
+            room_dic['name']=room.name
+            room_dic['description']=room.description
+            room_dic['owner']=room.owner.username
+            room_dic['updated_at']=room.updated_at.isoformat()
+            response.append(room_dic)
+        return HttpResponse(json.dumps(response))
+    else:
+        return HttpResponseBadRequest()
