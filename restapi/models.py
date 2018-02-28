6ce6071aabaea98cb612c92c397cc624dd902c28
PMDRose <shiro@ppy.sh>
2018-02-28 12:07:52 +0100

    add user test

--- restapi/models.py
+++ restapi/models.py
@@ -1,4 +1,9 @@
+import datetime
+from django.apps import apps
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+for app in apps.get_app_configs():
+    for model in app.get_models():
+        pass
--- restapi/tests.py
+++ restapi/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from . import models
 
 # Create your tests here.
+c = Client()
+
+class UserTest(TestCase):
+
+    def test_registration(self):
+        user_count=models.User.objects.count()
+        response=c.post('/register/', {'username':'testname','password1':'abcdefgh1234','password2':'abcdefgh1234'})
+        self.assertEqual(response.status_code,201)
+        self.assertEqual(models.User.objects.count(),user_count+1)
